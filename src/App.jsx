29b1080ececd0ab11419f43dd9c919921fc1149e
JerryCodesAfrica <egemonyeemelie@gmail.com>
2024-09-13 16:15:21 +0100

    made the button disabled when the initial API request hasn't been responded to

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// import { useEffect } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // setInterval(()=>fetchData(),3000)
 
@@ -8,6 +6,7 @@ function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [disabled, setDisable] = useState(false);
 
   // asyc function to get API data and store in state variable advice
   const fetchData = async () => {
@@ -21,6 +20,7 @@ function App() {
       console.log("Error:", error);
     } finally {
       setLoading(true);
+      setDisable(false);
     }
   };
 
@@ -32,12 +32,14 @@ function App() {
   const update = () => {
     setLoading(false)
     fetchData();
+    //disable button till respond is received
+    setDisable(true)
   };
 
   return (
     <div className="flex justify-center items-center bg-[#212633] lg:w-screen h-screen lg:h-screen">
       <div className="bg-[#303A48] relative flex justify-evenly items-center flex-col px-4 lg:w-2/5 w-3/4 h-1/4 lg:h-2/5 rounded-xl">
-        <h4 className="text-[#53FFAB]">ADVICE#{count}</h4>  
+        <h4 className="text-[#53FFAB]">ADVICE# {count}</h4>  
         <h2 className="text-white text-sm lg:text-xl text-center font-bold">
             {loading ? <span>{advice}</span> : <PreLoader />}
           </h2>
@@ -46,13 +48,15 @@ function App() {
           alt="divider"
           className="lg:py-4 py-6"
         />
-        <a
+        {/* This is the button */}
+        <button
           href="#"
           onClick={update}
-          className="absolute bg-[#53FFAB] p-4 rounded-full z-50 -bottom-6"
+          disabled={disabled}
+          className="absolute bg-[#53FFAB] p-4 rounded-full z-50 -bottom-6 disabled:opacity-80"
         >
           <img src="/images/icon-dice.svg" className="" alt="" />
-        </a>
+        </button>
       </div>
     </div>
   );
